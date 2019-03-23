726210515bb4f8a618fa41e9decdfa6e349f7c11
BAN951 <benji.adam.n9@gmail.com>
2019-03-23 12:32:08 +0100

    Added some functionality, output listing needs to be formatted with tables

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import json
 import sys
+import datetime
 
 
 def command(user_input, todos):
+    """
+    Command classifier function.
+    Depending on the input redirects a user to one
+    or to another functionality.
+    """
     if user_input == 'add':
         add_todo(todos)
     elif user_input == 'remove':
         remove_todo(todos)
+    elif user_input == 'done':
+        mark_done(todos)
     elif user_input == 'list':
         list_todos(todos)
     elif user_input == 'exit':
@@ -15,11 +23,13 @@ def command(user_input, todos):
 
 
 def add_todo(todos):
+    """ Adds a To-do to the registry. """
     title = input("\t Title: ")
     description = input('\t Description: ')
     todo = dict()
     todo['title'] = title
     todo['description'] = description
+    todo['done'] = False
     todos.append(todo)
     write_todos(todos)
 
@@ -28,6 +38,17 @@ def remove_todo(todos):
     # manage exceptions for string inputs
     index = int(input('\t To remove a TO-DO you must indicate it\'s index: '))
     del todos[index]
+    write_todos(todos)
+
+
+def mark_done(todos):
+    index = int(input('\t To update a TO-DO you must indicate it\'s index: '))
+    todo = todos[index]
+    if todo['done'] != True:
+        todo['done'] = True
+        write_todos(todos)
+    else:
+        print(' -- ', 'This TO-DO is already done', ' -- ')
 
 
 def write_todos(todos):
@@ -39,15 +60,33 @@ def write_todos(todos):
 def load_todos():
     with open('todos.json') as f:
         return json.load(f)
-    
+
+def print_format_todo(index, todo):
+    """
+    Takes in the index and an array a To-do
+    and returns the formatted string to print it.
+    """
+    print('\t::INDEX::', index)
+    print('\t::TITLE::', todo['title'])
+    print('\t::DESCRIPTION::\n\t\t', todo['description'])
+    if todo['done'] is False:
+        print('\t::STATUS:: - Not Finished')
+    else:
+        print('\t::STATUS:: - Finished') 
+    print('  ','-'*56)
+
 
 def list_todos(todos):
+    """ Lists all the To-dos registered """
     for index, todo in enumerate(todos):
-        print('Index:', index, '--', todo)
+        print_format_todo(index, todo)
 
 
 def main():
-    print("Welcome to the console TO-DO app!")
+    """
+    Main entry point of the program.
+    """
+    print("\nWelcome to the console TO-DO app!")
     print("=" * 80)
     print("To exit the program enter: exit")
 
